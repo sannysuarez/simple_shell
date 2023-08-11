e9f628ce7ff254edcd133a111e5941daa47c0d3c
swatty23 <swattymovine@gmail.com>
2023-08-11 03:25:02 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -53,11 +53,9 @@ typedef struct builtins
 	int (*function)(data_of_program *data);
 } builtins;
 
-
 /***** MAIN FUNCTIONS *****/
 
-
-/ ========  shell.c  ======== /
+/ == == == == shell.c == == == == /
 
 /* Inicialize the struct with the info of the program */
 void inicialize_data(data_of_program *data, int arc, char *argv[], char **env);
@@ -68,7 +66,7 @@ void sisifo(char *prompt, data_of_program *data);
 /* Print the prompt in a new line */
 void handle_ctrl_c(int opr UNUSED);
 
-/ ========  _getline.c  ======== /
+/ == == == == _getline.c == == == == /
 
 /* Read one line of the standar input */
 int _getline(data_of_program *data);
@@ -76,7 +74,7 @@ int _getline(data_of_program *data);
 /* split the each line for the logical operators if it exist */
 int check_logic_ops(char *array_commands[], int i, char array_operators[]);
 
- /======== expansions.c ========/
+/ == == == == expansions.c == == == == /
 
 /* expand variables */
 void expand_variables(data_of_program *data);
@@ -87,7 +85,7 @@ void expand_alias(data_of_program *data);
 /* append the string to the end of the buffer */
 int buffer_add(char *buffer, char *str_to_add);
 
- /======== str_tok.c ========/
+/ == == == == str_tok.c == == == == /
 
 /* Separate the string in tokens using a designed delimiter */
 void tokenize(data_of_program *data);
@@ -95,17 +93,17 @@ void tokenize(data_of_program *data);
 /* Creates a pointer to a part of a string */
 char *_strtok(char *line, char *delim);
 
- /======== execute.c ========/
+/ == == == == execute.c == == == == /
 
 /* Execute a command with its entire path */
 int execute(data_of_program *data);
 
-/ ======== builtins_list.c ======== /
+/ == == == == builtins_list.c == == == == /
 
 /* If match a builtin, executes it */
 int builtins_list(data_of_program *data);
 
-/ ======== find_in_path.c ======== /
+/ == == == == find_in_path.c == == == == /
 
 /* Creates an array of the path directories */
 char **tokenize_path(data_of_program *data);
@@ -115,7 +113,7 @@ int find_program(data_of_program *data);
 
 /***** HELPERS FOR MEMORY MANAGEMENT *****/
 
-/ ======== helpers_free.c ======== /
+/ == == == == helpers_free.c == == == == /
 
 /* Frees the memory for directories */
 void free_array_of_pointers(char **directories);
@@ -126,11 +124,9 @@ void free_recurrent_data(data_of_program *data);
 /* Free all field of the data */
 void free_all_data(data_of_program *data);
 
-
 /***** BUILTINS *****/
 
-
-/======== builtins_more.c ========/
+/ == == == == builtins_more.c == == == == /
 
 /* Close the shell */
 int builtin_exit(data_of_program *data);
@@ -147,8 +143,7 @@ int builtin_help(data_of_program *data);
 /* set, unset and show alias */
 int builtin_alias(data_of_program *data);
 
-
-/======== builtins_env.c ========/
+/ == == == == builtins_env.c == == == == /
 
 /* Shows the environment where the shell runs */
 int builtin_env(data_of_program *data);
@@ -159,11 +154,9 @@ int builtin_set_env(data_of_program *data);
 /* delete a variable of environment */
 int builtin_unset_env(data_of_program *data);
 
-
 /***** HELPERS FOR ENVIRONMENT VARIABLES MANAGEMENT *****/
 
-
-/======== env_management.c ========/
+/ == == == == env_management.c == == == == /
 
 /* Gets the value of an environment variable */
 char *env_get_key(char *name, data_of_program *data);
@@ -177,11 +170,9 @@ int env_remove_key(char *key, data_of_program *data);
 /* prints the current environ */
 void print_environ(data_of_program *data);
 
-
 /***** HELPERS FOR PRINTING *****/
 
-
-/======== helpers_print.c ========/
+/ == == == == helpers_print.c == == == == /
 
 /* Prints a string in the standar output */
 int _print(char *string);
@@ -192,11 +183,9 @@ int _printe(char *string);
 /* Prints a string in the standar error */
 int _print_error(int errorcode, data_of_program *data);
 
-
 /***** HELPERS FOR STRINGS MANAGEMENT *****/
 
-
-/======== helpers_string.c ========/
+/ == == == == helpers_string.c == == == == /
 
 /* Counts the number of characters of a string */
 int str_length(char *string);
@@ -213,8 +202,7 @@ char *str_concat(char *string1, char *string2);
 /* Reverse a string */
 void str_reverse(char *string);
 
-
-/======== helpers_numbers.c ========/
+/ == == == == helpers_numbers.c == == == == /
 
 /* Cast from int to string */
 void long_to_string(long number, char *string, int base);
@@ -225,8 +213,7 @@ int _atoi(char *s);
 /* count the coincidences of character in string */
 int count_characters(char *string, char *character);
 
-
-/======== alias_management.c ========/
+/ == == == == alias_management.c == == == == /
 
 /* print the list of alias */
 int print_alias(data_of_program *data, char *alias);
